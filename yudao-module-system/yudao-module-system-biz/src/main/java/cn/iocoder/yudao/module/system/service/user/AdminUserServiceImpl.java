48bbe3957dddc741eda2b3494968fa4caa50a062
yy2205 <2238220225@qq.com>
2024-11-13 16:21:15 +0800

    修改密码时修改MD5

--- yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/service/user/AdminUserServiceImpl.java
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/service/user/AdminUserServiceImpl.java
@@ -212,6 +212,7 @@ public class AdminUserServiceImpl implements AdminUserService {
         AdminUserDO updateObj = new AdminUserDO();
         updateObj.setId(id);
         updateObj.setPassword(encodePassword(password)); // 加密密码
+        updateObj.setPwdMd5(md5Encrypt(password));
         userMapper.updateById(updateObj);
 
         // 3. 记录操作日志上下文
